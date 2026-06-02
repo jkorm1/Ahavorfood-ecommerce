dee9d775043be92e20931fb1deee52a955ced359
jkorm1 <josephkorm1@gmail.com>
2026-06-02 11:42:40 +0000

    cloudflare mechanisms

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,14 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 export default {
-  default: {},
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 } satisfies OpenNextConfig;
